dd70a4957b40738a61b83764027cffe61082c329
Hüseyin Yalçın <hsyndvlpr@gmail.com>
2026-02-13 18:01:26 +0300

    Delete controller.js

--- controller.js
+++
@@ -1,9 +0,0 @@
-const terminateProcess = require("./useCase")
-const pumps = require("./pumps.json")
-const utils = require("./utils")
-let app_name = ["firefox","chrome"]
-
-
-
- terminateProcess(pumps, utils, app_name)
-
